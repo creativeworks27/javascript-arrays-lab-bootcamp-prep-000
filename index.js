1b2c745f75f7a5e83f301713b614189c636cf9eb
Roberto Romero <creativeworks27@icloud.com>
2018-09-17 18:58:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your functions and code here
 var kittens = ['Milo', 'Otis', 'Garfield'];
-function destructivelyAppendKitten(kittens){
+function destructivelyAppendKitten(name){
   kittens.push('Ralf');
   return kittens;
 }
